b3d2c4a1538e119aa0996c7577823da8d5dbc402
Ryan Yep <ryan.yep@gmail.com>
2016-01-19 20:22:24 -0600

    Fixed syntax error in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
     first_name: Faker::Name.first_name,
      last_name: Faker::Name.last_name,
       username: "#{Faker::Internet.user_name}#{x}",
-         email: Faker::Internet.user_name + i.to_s +
+         email: Faker::Internet.user_name + x.to_s +
                 "@#{Faker::Internet.domain_name}")
 end
